0e02cedf5e645fd884d1eb5462d127ca7d8b1636
EllaMoses <emoses@tulane.edu>
2023-11-07 22:45:28 -0600

    Code and Answers

--- answers.md
+++ answers.md
@@ -1,8 +1,8 @@
 # CMPS 2200 Recitation 06
 ## Answers
 
-**Name:**_________________________
-**Name:**_________________________
+**Name:** Ella Moses
+
 
 
 Place all written answers from `recitation-07.md` here for easier grading.
@@ -11,10 +11,35 @@ Place all written answers from `recitation-07.md` here for easier grading.
 
 - **2)**
 
+Recurrence of work:
+
+W(n) = W(n-1) + W(n-2) + 1
+
+W(n) = O(2^n)
+
 - **3)**
+
+Recurrence of span:
+
+S(n) = S(n-1) + S(n-2) + 1
+
+S(n) = O(2^n)
 
 - **4)**
 
+For a value n, counts has n+1 terms. counts[1:] is equal to the reverse fibonacci sequence for n and counts[0] is equal to the n-1 element of the fibonacci sequence.
+
 - **6)**
 
+The maximum number of times that fib_top_down(i)will be called for any value i is 2. 
+
+W(n) = O(n)
+S(n) = O(n)
+
+
 - **8)**
+
+Maximum number of times that Fi will be read is 2 because it will be read when n = i + 1 and n  i - 1. 
+
+W(n) = O(n)
+S(n) = O(n)
--- main.py
+++ main.py
@@ -4,24 +4,41 @@ def fib_recursive(n, counts):
     counts is a list of n+1 elements, where counts[i] is incremented
     each time fib_recursive(i, counts) is called.
     """    
+    counts[n] += 1 #keeps track of how many times fib_recursive(n, counts) is called for each n
+    if n == 0: #base case, if n is 0, fib value is 0
+        return 0
+    elif n== 1: #base case, if n is 1, fib value is 1
+        return 1
+    else: #recursive case, if n >= 2, the fib value is the sum of the previous 2 elements
+        return fib_recursive(n-1, counts) + fib_recursive(n-2, counts)
+    
+ 
+def fib_top_down(n, fibs, counts):
     counts[n] += 1
-    ###TODO
-    pass
-    
+    if fibs[n] != -1: #fibs is initialized with all -1s so if the nth term of fibs is not -1 it has already been found
+        return fibs[n] 
+    elif n == 0: #base case, if n is 0, fib value is 0
+        fibs[n] = 0
+    elif n == 1: #base case, if n is 1, fib value is 1
+        fibs[n] = 1
+    else: #recursive case: the nth term of the fibonacci sequence is equal to the sum of the previous two terms
+        fibs[n] = fib_top_down(n-1,fibs, counts) + fib_top_down(n-2,fibs, counts)
+    return fibs[n]
 
-    
-def fib_top_down(n, fibs):
-    ###TODO
-    pass
+n = 87
+counts = [0] * (n+1)
+fibs = [-1] * (n+1)
+fib_top_down(n, fibs, counts)
+print(counts)
+print(sum(counts))
 
 
 def fib_bottom_up(n):
-    ###TODO
-    pass
+    list = [0] * (n+1) #create a list with n+1 0's
+    if n >= 1: # if n is greater than 1, set the second term in the list to 1
+        list[1] = 1
+    for i in range(2, n+1): #for every value after the first 2 values, the fibonacci sequence is equal to the sum of the previous two terms
+        list[i] = list[i - 1] + list [i-2]
+    return list[-1] #return the final value in the list, the nth fibonacci number
 
-
-
-
-def fib_bottom_up_better(n):
-    ###TODO
-    pass
+ 
